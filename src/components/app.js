960e79cedaa7f5ec6ec98f7c63aff0d005b9f766
twalk2 <2twalk2@gmail.com>
2019-10-02 15:44:08 -0600

    added portfolio container and item

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import moment from "moment"
+import PortfolioContainer from './portfolio/portfolio-container';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Trevor Walker's Portfolio</h1>
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
+        <PortfolioContainer/>
+        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
       </div>
     );
   }
---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+import PortfolioItem from "./portfolio-item";
+
+export default class PortfolioContainer extends Component {
+    constructor() {
+        super()
+        console.log("Portfolio container has rendered")
+    }
+    render() {
+        return (
+            <div>
+                <h2>Portfolio items go here updated...</h2>
+                
+                <PortfolioItem/>
+            </div>
+        )
+    }
+}
