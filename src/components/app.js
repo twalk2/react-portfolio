0038bdf2844628cda348c5254ad9a1658a714a6c
twalk2 <2twalk2@gmail.com>
2019-10-04 10:03:07 -0600

    added route and switch

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
 import moment from "moment"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
 
 import PortfolioContainer from './portfolio/portfolio-container'
 import NavigationContainer from './navigation/navigation-container'
+import Home from './pages/Home'
+import About from './pages/About'
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <Router>
+          <div>
         <NavigationContainer />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+        </Switch>
+          </div>
+        </Router>
+
         <h1>Trevor Walker's Portfolio</h1>
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
         <PortfolioContainer/>
