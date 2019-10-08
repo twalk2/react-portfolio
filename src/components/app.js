76f9827d1edf4a84bd04a3b24a657df8dc513ff4
twalk2 <2twalk2@gmail.com>
2019-10-08 13:31:34 -0600

    added auth component and img

--- src/components/app.js
+++ src/components/app.js
@@ -1,39 +1,38 @@
-import React, { Component } from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom"
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import NavigationContainer from './navigation/navigation-container'
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Blog from './pages/Blog'
-import PortfolioDetail from './portfolio/portfolio-detail'
-import NoMatch from './pages/no-match'
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Blog from "./pages/Blog";
+import PortfolioDetail from "./portfolio/portfolio-detail";
+import Auth from "./pages/auth";
+import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
-
   render() {
     return (
-      <div className='container'>
+      <div className="container">
         <Router>
           <div>
             <NavigationContainer />
             <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/about' component={About} />
-              <Route path='/contact' component={Contact} />
-              <Route path='/blog' component={Blog} />
-              <Route exact path='/portfolio/:slug' component={PortfolioDetail} />
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route path="/auth" component={Auth} />
+              <Route
+                exact
+                path="/portfolio/:slug"
+                component={PortfolioDetail}
+              />
               <Route component={NoMatch} />
             </Switch>
           </div>
         </Router>
       </div>
     );
-
   }
 }
-
