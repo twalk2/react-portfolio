12487dd8c837c06a02b523aa75406ad81bd82263
twalk2 <2twalk2@gmail.com>
2019-11-01 15:56:58 -0600

    changed about image

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import profilePicture from "../../../static/assets/images/bio/snowy_mtns.jpg";
+import profilePicture from "../../../static/assets/images/bio/IMG_1360.jpg";
 
 const AboutComponent = () => {
   return (
@@ -13,13 +13,8 @@ const AboutComponent = () => {
         }}
       />
       <div className="right-side">
-        fsgfds dfsg kdhfs gh;fdsh ;hsdfh sd;fjldshgjl;dhfs;hg hj ;lsdh
-        lghl;dfh;lf hl;fdhjl;dshl ;dfhs lfh jdfhs jhj ldfghl ghjlfdhljdshldfghl
-        dhl dfhl dfghl fdghl dfhl hl fdshl fdgshl kfdshlk fdgklh fgdsklfgdklh
-        fdsgklhfdsgkl fkl dfdk skhl fdskhl fdsgkl fgdklhkldsfgkldfgkfgk hl;dfgsk
-        .n j;jdghdjfg;ljhkdfh fdjghl;fdkgjh; fjgg;dfj h;fgj h;dlhkj fl;djh
-        fdgkhjfgd k;j dfjh fgdj fgj f; lkfgdl; jssjjfdlgjs'o' jidhj p dsj'bjsdj
-        gdfjpo'djsg;jdsf;g dfsg;
+        Hey there! Glad to see you made it. Let me tell you a little about
+        myself!
       </div>
     </div>
   );
